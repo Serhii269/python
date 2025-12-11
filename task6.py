22a50f872d2e0bfd833f68e95b68d98c1281f138
Serhii269 <podgorodeckojsega@gmail.com>
2025-12-11 16:29:21 +0100

    indents

--- task6.py
+++ task6.py
@@ -1,4 +1,4 @@
 length = int(input("Enter length"))
 width = int(input("Enter width"))
-perimeter = (length+width) *2
+perimeter = (length + width) * 2
 print("The perimeter of the rectangle is equal to:", perimeter)
