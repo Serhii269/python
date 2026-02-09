81a20896b64db71b8caa21b70f978de54b84e622
Serhii269 <podgorodeckojsega@gmail.com>
2026-02-09 19:54:15 +0100

    task 28-31

---
+++ task30.py
@@ -0,0 +1,10 @@
+def popular_words(text, words):
+    text_lower = text.lower()
+    text_words = text_lower.split()
+    result = {word: text_words.count(word) for word in words}
+    return result
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', 
+                     ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
+
+print('OK')
